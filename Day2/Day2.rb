309081c8595d289b2f0aae3e2d9dd6dc53c0da5f
ginabak <gina.bak@shopify.com>
2019-12-03 20:10:25 -0500

    Setup for value replacement process

--- Day2/Day2.rb
+++ Day2/Day2.rb
@@ -1,33 +1,36 @@
 def main
 
-  opcode_hash = {
-    1 => {},
-    2 => {},
-    99 => {},
-  }
   array = []
   sequence = File.open("Day2Text.txt")
   sequence.each do |line|
     array << line.split(",")
   end
-  
+
   #note to self: flatten() makes it into a 1D array :) 
   array.flatten!
   
-  #create new_array to store new values :) 
-  new_array = Array.new((array.length),0)
-
-  temporary_index = 0 
+  #create duplicate with new_array to store new values :) 
+  new_array = []
+  new_array.replace(array)
+  
+  first_temporary_index = 0 
+  second_temporary_index = 0
+  third_temporary_index = 0
+  storing_value = 0
 
   # iterating through the array (with index value :))
   array.each_with_index do |num, index|
+
     # potential super nested ternary ... lol, probs not a good idea: 
     # (opcode_hash.key?(num))? (((num ==1)? ( puts 'add')) : ((num == 2)? ( puts 'multiply') : (break))) : ()
+    
+    first_temporary_index = array[index+1]
+    second_temporary_index = array[index+2]
+    third_temporary_index = array[index+3]
     case num
     when 1
-      temporary_index = index
-      temporary_index += 1
-      array[temporary_index]
+      storing_value = array[first_temporary_index] + array[second_temporary_index]
+      new_array[third_temporary_index] << storing_value
     when 2
       ''
     when 99
